ce753e97c3a133b7c6d207c2e43fe63489a312f9
Chisom Maxwell <chiscomax2013@gmail.com>
2022-02-05 14:47:26 -0800

    Added Old Paths as comment

--- src/config/constants/app_constants.js
+++ src/config/constants/app_constants.js
@@ -1,3 +1,6 @@
-export const BASE_URL = "http://localhost/bioafrique_api/public/api/v1/";
+export const BASE_URL = "http://localhost:8000/api/v1/";
 export const FLUTTERWAVE_CODE = 1;
 export const PAYSTACK_CODE = 2;
+
+//http://localhost:8000/api/v1/
+//http://localhost/bioafrique_api/public/api/v1/
