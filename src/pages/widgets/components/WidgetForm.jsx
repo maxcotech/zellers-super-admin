350bba68d1c20986d1f5014986e56b804db7570d
Chisom Maxwell <chiscomax2013@gmail.com>
2023-08-21 16:27:19 +0100

    Add Device Types to admin

--- src/pages/widgets/components/WidgetForm.jsx
+++ src/pages/widgets/components/WidgetForm.jsx
@@ -1,47 +1,48 @@
 import { CButton, CFormGroup, CLabel } from "@coreui/react";
 import { useState } from "react";
 import Spinner from "src/components/Spinner";
-import PropTypes  from "prop-types";
+import PropTypes from "prop-types";
 
 const WidgetForm = (props) => {
-    const [loading,setLoading] = useState(false);
-    const [formState,setFormState] = useState(props.defaultData ?? {
-        widget_title:"",
-        widget_link_text:"",
-        widget_link_address:"",
-        widget_type:"",
-        is_block:""
+    const [loading, setLoading] = useState(false);
+    const [formState, setFormState] = useState(props.defaultData ?? {
+        widget_title: "",
+        device_type: "",
+        widget_link_text: "",
+        widget_link_address: "",
+        widget_type: "",
+        is_block: ""
     });
-    const setFormValue = (e,key) => {
+    const setFormValue = (e, key) => {
         setFormState({
             ...formState,
-            [key]:e.target.value
+            [key]: e.target.value
         })
     }
     const onSubmit = () => {
-        props.submitHandler(formState,setLoading);
+        props.submitHandler(formState, setLoading);
     }
-    
+
     return (
         <>
             <form>
                 <CFormGroup>
                     <CLabel>Widget Title:</CLabel>
-                    <input onChange={(e) => setFormValue(e,"widget_title")} value={formState.widget_title} placeholder="Enter Widget Title"  className="form-control" />
+                    <input onChange={(e) => setFormValue(e, "widget_title")} value={formState.widget_title} placeholder="Enter Widget Title" className="form-control" />
                     <small className="form-text text-muted">
                         A text that will appear at the top of the widget. <b>Eg: Trendy Shoes below 40usd</b>
                     </small>
                 </CFormGroup>
                 <CFormGroup>
                     <CLabel>Widget Link Text:</CLabel>
-                    <input onChange={(e) => setFormValue(e,"widget_link_text")} value={formState.widget_link_text} placeholder="Enter Link Text"  className="form-control" />
+                    <input onChange={(e) => setFormValue(e, "widget_link_text")} value={formState.widget_link_text} placeholder="Enter Link Text" className="form-control" />
                     <small className="form-text text-muted">
                         A clickable text that will appear at the base of the widget. <b>Eg: Learn More...</b>
                     </small>
                 </CFormGroup>
                 <CFormGroup>
                     <CLabel>Widget Link Address:</CLabel>
-                    <input onChange={(e) => setFormValue(e,"widget_link_address")} value={formState.widget_link_address} placeholder="Enter Link Address"  className="form-control" />
+                    <input onChange={(e) => setFormValue(e, "widget_link_address")} value={formState.widget_link_address} placeholder="Enter Link Address" className="form-control" />
                     <small className="text-muted form-text">
                         The address to the page that the browser navigates to when the user clicks the link text.\n
                         It also acts as a fallback url when the individual widget items does not specify specific urls.
@@ -50,7 +51,7 @@ const WidgetForm = (props) => {
                 </CFormGroup>
                 <CFormGroup>
                     <CLabel>Widget Type:</CLabel>
-                    <select onChange={(e) => setFormValue(e,"widget_type")} value={formState.widget_type} className="form-control" >
+                    <select onChange={(e) => setFormValue(e, "widget_type")} value={formState.widget_type} className="form-control" >
                         <option value="">Select Widget Type</option>
                         <option value="1">Single Item Widget</option>
                         <option value="4">Four Item Widget</option>
@@ -61,18 +62,30 @@ const WidgetForm = (props) => {
                     </small>
                 </CFormGroup>
                 <CFormGroup>
+                    <CLabel>Supported Device:</CLabel>
+                    <select onChange={(e) => setFormValue(e, "device_type")} value={formState.device_type} className="form-control" >
+                        <option value="">Select Device</option>
+                        <option value="1">Mobile Applications only</option>
+                        <option value="2">Desktop and website applications only</option>
+                        <option value="3">All Devices</option>
+                    </select>
+                    <small className="text-muted form-text">
+                        This determines the type of App / Display where this widget will be rendered.
+                    </small>
+                </CFormGroup>
+                <CFormGroup>
                     <CLabel>Element Type:</CLabel>
-                    <select onChange={(e) => setFormValue(e,"is_block")} value={formState.is_block}  className="form-control" >
+                    <select onChange={(e) => setFormValue(e, "is_block")} value={formState.is_block} className="form-control" >
                         <option value="">Select Element Type</option>
                         <option value="0">Inline Element</option>
                         <option value="1">Block Level Element</option>
                     </select>
                     <small className="text-muted form-text">
                         Determines whether this widget can have other widgets in the same row.
-                        Inline widgets can accomodate other inline widgets in the same row, while block widgets can not. 
+                        Inline widgets can accomodate other inline widgets in the same row, while block widgets can not.
                     </small>
                 </CFormGroup>
-        
+
                 <CButton block={false} onClick={onSubmit} color="primary"><Spinner status={loading} /> {props.btn_title ?? "Continue"}</CButton>
 
             </form>
@@ -81,9 +94,9 @@ const WidgetForm = (props) => {
 }
 
 WidgetForm.propTypes = {
-    submitHandler:PropTypes.func.isRequired,
-    defaultData:PropTypes.object,
-    btn_title:PropTypes.string
+    submitHandler: PropTypes.func.isRequired,
+    defaultData: PropTypes.object,
+    btn_title: PropTypes.string
 }
 
 export default WidgetForm;
